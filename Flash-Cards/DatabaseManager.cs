7ed52770ca63edca05063686fa3e0cf0ad0e77b5
Sundebergius <1089441@ucn.dk>
2022-10-18 10:55:16 +0200

    first commit

---
+++ Flash-Cards/DatabaseManager.cs
@@ -0,0 +1,81 @@
+using System; 
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+
+namespace flashcards
+{
+    internal class DatabaseManager
+    {
+        internal static void CheckDatabase()
+        {
+            string connectionString = "Server=(localdb)\\MSSQLLocalDB;Intergrated Security=true;";
+            try
+            {
+                SqlConnection conn = new SqlConnection(connectionString);
+                using (conn)
+                {
+                    conn.Open();
+                    var tableCmd = conn.CreateCommand();
+                    tableCmd.CommandText =
+                        $@"IF NOT EXISTS (SELECT * from sys.databases WHERE name = 'quizDb')
+                            BEGIN
+                                CREATE DATABASE quizDb;
+                            END;
+                        ";
+                    tableCmd.ExecuteNonQuery();
+                    conn.Close();
+                }
+
+                CreateTables();
+            }
+            catch (exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        internal static void CreateTables()
+        {
+            string connectionString = "Server=(localdb)\\MSSQLLocalDB; Initial Catalog=quizDb; Integrated Security=true;";
+            SqlConnection conn = new SqlConnection(connectionString);
+            using (conn)
+            {
+                conn.Open();
+                var tableCmd = conn.CreateCommand();
+
+                tableCmd.CommandText = 
+                    $@" IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'stack')
+                        CREATE TABLE stack (
+                                Id int IDENTITY(1,1) NOT NULL,
+                                Name varchar (100) NOT NULL UNIQUE,
+                                PRIMARY KEY (Id)
+                                );
+                            ";
+                        tableCmd.ExecuteNonQuery();
+
+                        tableCmd.CommandText =
+                            $@" IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = 'flashcard')
+                                CREATE TABLE flashcard (
+                                        Id int IDENTITY(1,1) NOT NULL PRIMARY KEY,
+                                        Question varchar(30) NOT NULL,
+                                        Answer varchar(30) NOT NULL,
+                                        StackId int NOT NULL
+                                            FOREIGN KEY
+                                            REFERENCES stack(Id)
+                                            ON DELETE CASCADE
+                                            ON UPDATE CASCADE
+                                        );
+                                    ";
+                                tableCmd.ExecuteNonQuery();
+
+                                conn.Close();
+
+                                Console.WriteLine("Good to go");
+
+            }
+        }
+    }
+}
---
+++ Flash-Cards/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading;
+using flashCards;
+using Microsoft.Data.SqlClient;
+
+namespace Flashcards
+{
+    class Program
+    {
+        static void Main()
+        {
+            DatabaseManager.CheckDatabase();
+            UserCommands.MainMenu();
+        }
+    }
+}
